bfc2ef60498dc79a102a62f3f5f3d40388fa0536
rudradcruze <francisrudra@gmail.com>
2024-03-31 02:29:08 +0600

    hide api key

--- Google-Gemini-App/gemini_utility.py
+++ Google-Gemini-App/gemini_utility.py
@@ -1,7 +1,8 @@
-import configparser
 import os
 import json
 import google.generativeai as genai
+from dotenv import load_dotenv
+
 
 # get the working directory
 working_directory = os.path.dirname(os.path.realpath(__file__))
@@ -10,10 +11,12 @@ config_file_path = f"{working_directory}/config.json"
 config_data = json.load(open(config_file_path))
 
 # loading the api key
-GOOGLE_API_KEY = config_data["GOOGLE_API_KEY"]
+load_dotenv()
+api_key = os.getenv('GOOGLE_API_KEY')
+
 
 # configuring google.generativeai with api key
-genai.configure(api_key=GOOGLE_API_KEY)
+genai.configure(api_key=api_key)
 
 
 # function to load gemini-pro-model for chatbot
